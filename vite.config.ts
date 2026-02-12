31d3e5b46da9d23d76d0f244f866fa0fe55fd650
Doğukan Yegin <124673557+dogukanyegin@users.noreply.github.com>
2026-02-12 23:10:34 +0300

    Update vite.config.ts
    Signed-off-by: Doğukan Yegin <124673557+dogukanyegin@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+export default defineConfig({
+  base: "/factshield/",   // 👈 Project Pages path'i
+  plugins: [react()],
+});
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    base: "/factshield/"
     return {
       server: {
         port: 3000,
