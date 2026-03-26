31709bd0dcd62d1dfcb90ca8913412832888e3e2
Doğukan Yegin <124673557+dogukanyegin@users.noreply.github.com>
2026-03-26 21:04:16 +0300

    Update vite.config.ts
    Signed-off-by: Doğukan Yegin <124673557+dogukanyegin@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   loadEnv(mode, process.cwd(), "");
 
   return {
-    base: mode === "production" ? "/factshield/" : "/",
+    base: mode === "production" ? "/nordicfactshield.norway/" : "/",
     plugins: [react()],
     server: { port: 3000, host: "0.0.0.0" },
     resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
