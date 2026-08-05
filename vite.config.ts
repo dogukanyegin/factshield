2da954d8a4663bea7c8ab5e7f095955e90065bae
Doğukan Yegin <124673557+dogukanyegin@users.noreply.github.com>
2026-08-05 14:34:14 +0300

    Simplify Vite configuration by removing loadEnv
    Removed loadEnv function and simplified configuration.
    
    Signed-off-by: Doğukan Yegin <124673557+dogukanyegin@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig(({ mode }) => {
-  loadEnv(mode, process.cwd(), "");
-
-  return {
-    base: mode === "production" ? "/nordicfactshield.norway/" : "/",
-    plugins: [react()],
-    server: { port: 3000, host: "0.0.0.0" },
-    resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-  };
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
